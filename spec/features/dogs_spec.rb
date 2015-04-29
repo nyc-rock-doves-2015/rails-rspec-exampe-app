45f3bb943fea51f4a38050cbb9982d5f3adf62c2
Steven Cassidy <steven@mumsnet.com>
2015-04-28 21:28:48 -0400

    Update spec to target a data- attribute

--- spec/features/dogs_spec.rb
+++ spec/features/dogs_spec.rb
@@ -4,7 +4,7 @@ RSpec.feature "Dog listing", :type => :feature do
   scenario "User creates the dog index page" do
     dog = FactoryGirl.create(:dog)
     visit "/dogs"
-    selector = "#dog_#{dog.id}"
+    selector = "[data-dog-id=\"#{dog.id}\"]"
     save_and_open_page
     expect(page).to have_selector(selector)
   end
